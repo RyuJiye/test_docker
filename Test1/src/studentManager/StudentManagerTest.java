95503a43b23ba453a8901a6d81beba3fac8e522a
JayChung0101 <chung5587@naver.com>
2025-05-28 11:15:16 +0900

    test: 임시 주석 추가

--- Test1/src/studentManager/StudentManagerTest.java
+++ Test1/src/studentManager/StudentManagerTest.java
@@ -35,7 +35,7 @@ class StudentManagerTest {
 	    });
 	}
 
-	@Test // 임시 주석
+	@Test // 임시 주석1
 	void testRemoveNonExistStudent() {
 		assertThrows(IllegalArgumentException.class, () -> {
 	        sm.removeStudent("Student1");
